7eab18aacc0922632c8eb41f462942dcc18b2e1a
Gustavo Oliveira <gustavooliveira.classroom@gmail.com>
2024-05-17 16:35:17 -0300

    Ajustes página de membros

--- lib/org_users.dart
+++ lib/org_users.dart
@@ -14,41 +14,80 @@ class _OrgUsersScreen extends State<OrgUsersScreen> {
         title: Text('Orgs'),
       ),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(7.0),
-          child: Stack(
-            children: [
-              Container(
-                height: 400,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/volunteer.jpg'),
-                    fit: BoxFit.cover,
-                    colorFilter: ColorFilter.mode(
-                      Colors.black.withOpacity(0.5),
-                      BlendMode.darken,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(7.0),
+              child: Stack(
+                children: [
+                  Container(
+                    height: 400,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/volunteer.jpg'),
+                        fit: BoxFit.cover,
+                        colorFilter: ColorFilter.mode(
+                          Colors.black.withOpacity(0.5),
+                          BlendMode.darken,
+                        ),
+                      ),
                     ),
                   ),
-                ),
+                  Center(
+                    child: Container(
+                      height: 400,
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Usuários - Organização 1',
+                        style: TextStyle(
+                          fontSize: 50.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              Center(
-                child: Container(
-                  height: 400,
-                  alignment: Alignment.center,
-                  child: Text(
-                    'Usuários - Organização 1',
+            ),
+            SizedBox(height: 50.0),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.6,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Todos os usuários da organização',
                     style: TextStyle(
-                      fontSize: 50.0,
-                      color: Colors.white,
+                      fontSize: 30.0,
                       fontWeight: FontWeight.bold,
                     ),
                     textAlign: TextAlign.center,
                   ),
-                ),
+                  SizedBox(height: 20.0),
+                  Container(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Ação do botão Convidar usuário
+                      },
+                      child: Text(
+                        'Convidar usuário',
+                        style: TextStyle(fontSize: 20.0, color: Colors.white),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        padding: EdgeInsets.symmetric(vertical: 20.0),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+            SizedBox(height: 50.0),
+          ],
         ),
       ),
     );
